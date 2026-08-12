cd5da0fdef0d5b4424f4f25d6a3d8692b372e7e7
Adam PC <ajm76389@uga.edu>
2026-08-11 17:46:20 -0700

    fix typo

--- src/receiver.py
+++ src/receiver.py
@@ -9,4 +9,4 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size max for testing
-    print(f"received message from clent {data}")
+    print(f"received message from client {data}")
