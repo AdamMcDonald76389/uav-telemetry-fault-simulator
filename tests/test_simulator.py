23239f0c9ce18930ca8f7e03eaa64dee0e16847f
Adam McDonald <ajm76389@uga.edu>
2026-08-22 01:45:37 -0700

    add testSend

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -30,4 +30,38 @@ def testCorrupt(monkeypatch):
     assert len(sentData) == len(data)
     assert sentData[0] == data[0] ^ 0x01
 
+def testLoss():
+    pass
+
+
+
+
+def testSend():
+    packet = telemetryData(
+        deviceName="UAV-002",
+        sequence = 1,
+        altitude= 5000,
+        speed = 330
+    )
+    mockSock = Mock()
+    targetAddress =("127.0.0.1", 500)
+
+
+
+    simulator.encodeAndSend(packet, mockSock, targetAddress)
+
+    sentData, sentAddress = mockSock.sendto.call_args.args
+    assert sentAddress == targetAddress
+    rawJson = sentData.decode("utf-8")
+    
+    sentPacket = telemetryData.model_validate_json(rawJson)
+    assert packet == sentPacket
+
+
+def testHold():
+    pass
+
+
+def testRepeat():
+    pass
 
