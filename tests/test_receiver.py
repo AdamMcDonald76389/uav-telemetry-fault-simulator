0fa9f19120e714f3510d5625d6e8b5f6be01a346
Adam PC <ajm76389@uga.edu>
2026-08-23 23:22:07 -0700

    add base case test

--- tests/test_receiver.py
+++ tests/test_receiver.py
@@ -1,20 +1,50 @@
 # test suite for receiver
 # tets valid and invalid input 
-
-
 import pytest
+from unittest.mock import Mock
+from src.telemetryData import telemetryData
+from src import receiver
 
-# correctly sent packet with no anomalies
+# helper function
+# data to decode for use in handle function
+# since takes encoded bytes over utf-8
+def bytePacket():
+    packet = telemetryData(
+        deviceName="UAV-001",
+        sequence=1,
+        altitude=5000,
+        speed = 300
+
+    )
+    data = packet.model_dump_json()
+    sendData = data.encode("utf-8")
+    return sendData
+
+
+@pytest.fixture
+def mockSock():
+    return Mock()
+
+
+
+# test correctly sent packet with no anomalies
 def testBaseCase():
-    pass
+    highest: dict[str, int] = {}
+    missingSequence: dict[str, set[int]] = {}
+    receiver.handlePacket(bytePacket(), highest, missingSequence)
 
+    assert highest["UAV-001"] == 1
+    assert missingSequence["UAV-001"] == set()
+
+# tests handling of duplicate logic
 def testHandleDuplicate():
     pass
 
 
-
+# tests handling of packet sent out of order 
 def testHandleLate():
     pass
 
-
-
+# tests handling of packets when the very first packet is out of order
+def testInitialGap():
+    pass
