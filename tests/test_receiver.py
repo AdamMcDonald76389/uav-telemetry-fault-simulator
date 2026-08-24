01c594ba1ebd7c36230128ba858096b0f1ed532a
Adam PC <ajm76389@uga.edu>
2026-08-23 23:45:35 -0700

    test receiver unit tests

--- tests/test_receiver.py
+++ tests/test_receiver.py
@@ -8,10 +8,10 @@ from src import receiver
 # helper function
 # data to decode for use in handle function
 # since takes encoded bytes over utf-8
-def bytePacket():
+def bytePacket(sequence):
     packet = telemetryData(
         deviceName="UAV-001",
-        sequence=1,
+        sequence=sequence,
         altitude=5000,
         speed = 300
 
@@ -27,18 +27,32 @@ def mockSock():
 
 
 
-# test correctly sent packet with no anomalies
-def testBaseCase():
+# test very first correctly sent packet with no anomalies
+def testInitialization():
     highest: dict[str, int] = {}
     missingSequence: dict[str, set[int]] = {}
-    receiver.handlePacket(bytePacket(), highest, missingSequence)
+    receiver.handlePacket(bytePacket(1), highest, missingSequence)
 
     assert highest["UAV-001"] == 1
     assert missingSequence["UAV-001"] == set()
 
+# tests receiving of packets besides the first packet
+def testBaseCase():
+    highest: dict[str, int] = {"UAV-001": 1}
+    missingSequence: dict[str, set[int]] = {"UAV-001": set()}
+
+    receiver.handlePacket(bytePacket(2), highest, missingSequence)
+
+    assert highest["UAV-001"] == 2
+    assert missingSequence["UAV-001"] == set()
+
 # tests handling of duplicate logic
-def testHandleDuplicate():
-    pass
+def testHandleDuplicate(capsys):
+    highest: dict[str, int] = {"UAV-001": 1}
+    missingSequence: dict[str, set[int]] = {"UAV-001": set()}
+    receiver.handlePacket(bytePacket(1), highest, missingSequence)
+    captured = capsys.readouterr()
+    assert "duplicate packet received" in captured.out
 
 
 # tests handling of packet sent out of order 
@@ -47,4 +61,9 @@ def testHandleLate():
 
 # tests handling of packets when the very first packet is out of order
 def testInitialGap():
+    pass
+
+
+# tests handling of packets that 
+def testHandleUnexpected():
     pass
